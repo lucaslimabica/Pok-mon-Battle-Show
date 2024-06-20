fbb690fad4725df86b5a7c61ac01fade8b833e2e
Lucas de Lima Bica <delimabica@gmail.com>
2024-06-20 01:42:11 +0100

    Level Integer
    Calculates the first section of the real demage.

--- main.py
+++ main.py
@@ -13,5 +13,11 @@ def isCritical():
     else:
         return 1
     
-for i in range(10):
-    print(f"Critical hit: {isCritical()}")
+def levelValue(level):
+    """
+    Returns the operation value based on the given level.
+    The level value is calculated by multiplying the level by 2 and by the critical value,
+    after divide by 2 and sum 2.
+    """
+    critical_value = isCritical()
+    return (level * 2 * critical_value) // 2 + 2
